27b1e002dd37e0af1fb5a473991f5fa47b3df376
VincenzoNucci <vincenzo.nucci@studenti.unicam.it>
2021-02-02 17:53:39 +0100

    save path and output dir

--- weights_extractor.py
+++ weights_extractor.py
@@ -2,6 +2,7 @@ import numpy as np
 import tensorflow as tf
 import pickle
 import os
+import shutil
 
 # This class helps in saving the weights of a Dense/Convolutional layer for each batch iteration during training
 # For Dense layer, saves only the weights and ignore the bias
@@ -13,7 +14,7 @@ class SaveCompressedWeightsNetwork(tf.keras.callbacks.Callback):
     else:
     	if os.listdir(output_dir): # Directory exists and is not empty -> output warning and continue
     	    print('Output directory already exists and is not empty, existing files will be overwritten.')
-    
+    self.output_dir = output_dir
   def on_train_begin(self, batch, logs=None):
     self.epochs = self.params.get('epochs')
     self.initial = {n:[] for n in range(len(self.model.layers))}
@@ -36,9 +37,9 @@ class SaveCompressedWeightsNetwork(tf.keras.callbacks.Callback):
     if epoch == 0: # Put weight initialization as first element in weights layer
       for n in range(len(self.model.layers)):
         self.weights_layer[n].insert(0,self.initial[n])
-    save_fname = str.zfill(epoch+1,len(str(self.epochs)))
+    save_fname = str.zfill(epoch+1,len(str(self.epochs))+1)
     
-    pickle.dump(self.weights_layer,open(f'{save_fname}.pkl','wb'))
+    pickle.dump(self.weights_layer,open(f'{os.path.join(self.output_dir,save_fname)}.pkl','wb'))
     # Reset arrays for layers' weight for next epoch
     for n in range(len(self.model.layers)):
       self.weights_layer[n] = []
