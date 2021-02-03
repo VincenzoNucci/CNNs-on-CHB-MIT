1852c9b5c6d41e9eefd2d6405039c96ba66d46fa
VincenzoNucci <vincenzo.nucci@studenti.unicam.it>
2021-02-03 10:40:15 +0100

    better mgmt initial weights

--- weights_extractor.py
+++ weights_extractor.py
@@ -17,26 +17,27 @@ class SaveCompressedWeightsNetwork(tf.keras.callbacks.Callback):
     self.output_dir = output_dir
   def on_train_begin(self, batch, logs=None):
     self.epochs = self.params.get('epochs')
-    self.initial = {n:1 for n in range(len(self.model.layers))}
+    self.initial = {n:[] for n in range(len(self.model.layers))}
     self.weights_layer = {n:[] for n in range(len(self.model.layers))}
     # Saves weight initialization
     for n in range(len(self.model.layers)):
       # Exclude layers which does not have weights (Dropout, Flatten, MaxPool...)
-      if self.model.layers[n].name in ['conv','dense']:
-        self.initial[n] *= self.model.layers[n].get_weights()[0]
+      if self.model.layers[n].name.find('conv') >=0 or self.model.layers[n].name.find('dense')>=0:
+        self.initial[n].append(self.model.layers[n].get_weights()[0])
     # pickle.dump(self.initial,open(f'initial.pkl','wb'))
 
   def on_train_batch_end(self, batch, logs=None):
     # Saves all layers' weights at the end of each batch
     for n in range(len(self.model.layers)):
-      if self.model.layers[n].name in ['conv','dense']:
+      if self.model.layers[n].name.find('conv') >=0 or self.model.layers[n].name.find('dense')>=0:
         self.weights_layer[n].append(self.model.layers[n].get_weights()[0])
   
   def on_epoch_end(self, epoch, logs=None):
     # Saves weights splitting per epochs. The first epoch will contain weights initialization at first position
     if epoch == 0: # Put weight initialization as first element in weights layer
       for n in range(len(self.model.layers)):
-        self.weights_layer[n].insert(0,self.initial[n])
+        if len(self.initial[n])>0:
+          self.weights_layer[n].insert(0,self.initial[n][0])
     save_fname = str.zfill(str(epoch+1),len(str(self.epochs))+1)
     
     pickle.dump(self.weights_layer,open(f'{os.path.join(self.output_dir,save_fname)}.pkl','wb'))
