3833427ceb5b810abb4e07b8d8eb6c1cb5d9e4f1
VincenzoNucci <vincenzo.nucci@studenti.unicam.it>
2021-02-04 18:19:10 +0100

    change data format to channels last for cpu

--- custom_model.py
+++ custom_model.py
@@ -3,22 +3,26 @@ import tensorflow.keras.layers as layers
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv3D, BatchNormalization, Flatten, Dropout, Dense
 
+rseed = 11246978
+df = 'channels_last' # if using cpu
+# df = 'channels_first' # if using gpu (remember to remove the transpose in the data generators)
+
 def createModel():
     input_shape=(1, 22, 59, 114)
     model = Sequential()
     #C1
-    model.add(Conv3D(16, (22, 5, 5), strides=(1, 2, 2), padding='valid',activation='relu',data_format= "channels_first", input_shape=input_shape))
-    model.add(layers.MaxPooling3D(pool_size=(1, 2, 2),data_format= "channels_first",  padding='same'))
+    model.add(Conv3D(16, (22, 5, 5), strides=(1, 2, 2), padding='valid',activation='relu',data_format= df, input_shape=input_shape))
+    model.add(layers.MaxPooling3D(pool_size=(1, 2, 2),data_format= df,  padding='same'))
     model.add(BatchNormalization())
     
     #C2
-    model.add(Conv3D(32, (1, 3, 3), strides=(1, 1,1), padding='valid',data_format= "channels_first",  activation='relu'))#incertezza se togliere padding
-    model.add(layers.MaxPooling3D(pool_size=(1,2, 2),data_format= "channels_first", ))
+    model.add(Conv3D(32, (1, 3, 3), strides=(1, 1,1), padding='valid',data_format= df,  activation='relu'))#incertezza se togliere padding
+    model.add(layers.MaxPooling3D(pool_size=(1,2, 2),data_format= df, ))
     model.add(BatchNormalization())
     
     #C3
-    model.add(Conv3D(64, (1,3, 3), strides=(1, 1,1), padding='valid',data_format= "channels_first",  activation='relu'))#incertezza se togliere padding
-    model.add(layers.MaxPooling3D(pool_size=(1,2, 2),data_format= "channels_first", ))
+    model.add(Conv3D(64, (1,3, 3), strides=(1, 1,1), padding='valid',data_format= df,  activation='relu'))#incertezza se togliere padding
+    model.add(layers.MaxPooling3D(pool_size=(1,2, 2),data_format= df, ))
     model.add(BatchNormalization())
     
     model.add(Flatten())
